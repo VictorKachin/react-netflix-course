b397001feff8a976a640ae5ccbf46c89ef458768
Victor Kachin <victor.kachin@gmail.com>
2025-09-22 17:02:04 +0300

    added FavoriteButton witg useState hook

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,11 +1,19 @@
+import { FavoriteButton } from './assets/FavoriteButton'
+
 export function MovieCard({ image, rating }) {
 	return (
-		<div className='relative w-[200px] rounded-2xl overflow-hidden bg-neutral-900 shadow-lg'>
+		<div className='relative w-[200px] rounded-2xl overflow-hidden bg-neutral-900 shadow-lg hover:scale-105 transition-transform will-change-transform duration-300'>
 			<img
 				src={image}
 				alt='Movie Poster'
 				className='w-full h-auto object-cover'
 			/>
+			{/* button of favorite */}
+			<div className='absolute top-2 right-2 z-10'>
+				<FavoriteButton />
+			</div>
+
+			{/* gradient and rating */}
 			<div className='absolute bottom-0 left-0 w-full bg-gradient-to-t from-black/80 to-transparent p-2 text-sm text-white font-semibold'>
 				IMDb: {rating}
 			</div>
---
+++ src/assets/FavoriteButton.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+export function FavoriteButton() {
+	const [isFavorite, setIsFavorite] = useState(false)
+
+	return (
+		<button onClick={() => setIsFavorite(!isFavorite)}>
+			{isFavorite ? '❤️' : '🤍'}
+		</button>
+	)
+}
