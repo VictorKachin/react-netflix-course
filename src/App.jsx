bfe4e6a16c745771ec705cf3ca8f0be9c7035cbf
Victor Kachin <victor.kachin@gmail.com>
2025-09-30 19:08:31 +0300

    creating search input and useDebounce hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
+import { useState } from 'react'
+import { useDebounce } from './hooks/useDebounce'
 import { MovieCard } from './MovieCard'
 import { MOVIES } from './movies.data'
 
 function App() {
+	const [searchTerm, setSearchTerm] = useState('')
+	const debouncedSearch = useDebounce(searchTerm, 500)
+	const movies = MOVIES.filter(movie =>
+		movie.name.toLowerCase().includes(debouncedSearch.toLowerCase())
+	)
 	return (
 		<div className='min-h-screen w-full bg-black text-white px-6 py-5'>
-			<header className='mb-10 flex items-center'>
+			<header className='mb-10 flex items-center justify-between'>
 				<img src='/public/netflix.png' alt='Netflix' className='h-8 w-auto' />
+				<input
+					type='search'
+					name='searchTerm'
+					value={searchTerm}
+					onChange={e => {
+						setSearchTerm(e.target.value)
+					}}
+					placeholder='Search...'
+					className='border border-white/15 px-2 py-1 rounded'
+				/>
 			</header>
 			<main className='flex gap-6'>
-				{MOVIES.map(movie => (
-					<MovieCard
-						key={movie.name}
-						image={movie.image}
-						rating={movie.rating}
-					/>
-					// or
-					// <MovieCard {...movie} />
-				))}
+				{movies.length ? (
+					movies.map(movie => (
+						<MovieCard
+							key={movie.name}
+							image={movie.image}
+							rating={movie.rating}
+						/>
+						// or
+						// <MovieCard {...movie} />
+					))
+				) : (
+					<p>Movies not found</p>
+				)}
 			</main>
 		</div>
 	)
@@ -25,4 +46,4 @@ function App() {
 export default App
 
 // https://www.youtube.com/watch?v=UC2vnwCY4T4
-// 26:10
+// 36:12
--- src/FavoriteButton.jsx
+++ src/FavoriteButton.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export function FavoriteButton() {
 	const [isFavorite, setIsFavorite] = useState(false)
-
+	useEffect(() => {
+		console.log('Состояние изменилось!')
+	}, [isFavorite])
 	return (
 		<button onClick={() => setIsFavorite(!isFavorite)}>
 			{isFavorite ? '❤️' : '🤍'}
 		</button>
 	)
 }
+
+// https://www.youtube.com/watch?v=UC2vnwCY4T4
+// 29:04
